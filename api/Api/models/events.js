651754e7592ae24c259059bcffaf94118b1ef1f5
ishitarastogi <ishita3099@gmail.com>
2021-11-23 14:57:57 +0530

    Add schema

--- api/Api/models/events.js
+++ api/Api/models/events.js
@@ -2,6 +2,15 @@ const mongoose=require('mongoose')
 //layout
 const eventSchema=mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
+    EventName: String,
+    Timings:{type:Date, default: Date.now,required:true},
+    Venue: String,
+    Description: String,
+    Price:{type: Array,required:true},
+    Address:{type: String, required:true},
+    totalSupply:Number
+
+
     
 });
 module.exports=mongoose.model('Event',eventSchema);
