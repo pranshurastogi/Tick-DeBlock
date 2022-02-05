f51de3d18e9383903aa46c1e9b288c3976c814a2
ishitarastogi <ishita3099@gmail.com>
2022-02-05 23:27:47 +0530

    add pages and components

---
+++ client/src/pages/register/Register.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Register.css";
+
+function Register() {
+  return (
+    <div>
+      <div class="v51_92">
+        <div class="v52_93"></div>
+        <span class="v52_97">Tick-DeBlock</span>
+        <span class="v52_101">Welcome to Tick-DeBlock</span>
+        <span class="v52_102">Email</span>
+        <div class="v52_103"></div>
+        <div class="v52_104"></div>
+        <span class="v52_105">Create Password</span>
+        <div class="v52_106"></div>
+        <div class="v52_107"></div>
+        <span class="v52_108">Sign In</span>
+        <span class="v52_109">Sign Up</span>
+        <div class="name"></div>
+        <div class="name"></div>
+        <span class="v52_112">or</span>
+        <div class="v52_114"></div>
+        <div class="v52_115"></div>
+        <div class="v52_137"></div>
+        <span class="v52_138">acebook</span>
+        <div class="v52_113"></div>
+        <div class="v52_129">
+          <div class="name"></div>
+        </div>
+        <span class="v52_135">With Google</span>
+        <div class="v52_136"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
